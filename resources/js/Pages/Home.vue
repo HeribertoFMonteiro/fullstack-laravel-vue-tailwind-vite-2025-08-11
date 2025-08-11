542805f5034f6972a1142632bd9b33712110f26d
HeribertoFM <hfmk2015@gmail.com>
2025-08-11 14:05:10 +0900

    segundo commit - atualizando README

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold mb-4">Bem-vindo ao seu projeto Laravel + Inertia + Vue!</h1>
+    <p>Esta é a página inicial.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
